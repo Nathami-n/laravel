368af6c16462718fbeb9a984ad7fa88dedb6b676
Nathami-n <georgenathan010@gmail.com>
2024-06-14 16:25:15 +0300

    Add: extended the header and used the @section directive

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,1 +1,12 @@
+@extends('layout.layout')
+
+
+
+@section("content")
+<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cum neque vel cumque. Laudantium, mollitia, officiis dolorum modi nihil perferendis neque suscipit alias earum id fuga fugit harum placeat provident!</p>
+
+@endsection
+
+
+
 
